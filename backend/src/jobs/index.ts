b1815597dcef8332808f628d987a875cd2c0f128
Arifur Rahman Alvi <111847765+ninjatypegame@users.noreply.github.com>
2022-09-09 10:12:37 +0600

    Delete backend directory

--- backend/src/jobs/index.ts
+++
@@ -1,5 +0,0 @@
-import updateLeaderboards from "./update-leaderboards";
-import deleteOldLogs from "./delete-old-logs";
-import announceDailyLeaderboards from "./announce-daily-leaderboards";
-
-export default [updateLeaderboards, deleteOldLogs, announceDailyLeaderboards];
